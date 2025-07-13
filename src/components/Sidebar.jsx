a15285b92055a6d3d738b4e83de920d03f8ca9ca
zldn109 <ss771009@naver.com>
2025-07-13 18:18:03 +0900

    refactor: useNavigate 대신 <Link> 사용하여 수정

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import { isDayState, selectedCategoryState } from "@/store/rootAtoms";
 import styles from "@/styles/components/Sidebar.module.scss";
 import { useRecoilState, useRecoilValue } from "recoil";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   const categories = [
@@ -20,24 +20,22 @@ const Sidebar = () => {
   );
   const isDay = useRecoilValue(isDayState);
 
-  const navigate = useNavigate();
-
   return (
     <aside className={`${styles.sidebar} ${!isDay ? styles.nightSidebar : ""}`}>
       <ul className={styles.categoryList}>
         {categories.map((cat) => (
-          <li
-            key={cat}
-            className={`${styles.categoryItem} ${
-              selectedCategory === cat ? styles.active : ""
-            }`}
-            onClick={() => {
-              setSelectedCategory(cat);
-              navigate(`/${cat}`);
-            }}
-          >
-            {cat === "home" ? "Home" : cat}
-          </li>
+          <Link to={`/${cat}`} key={cat} className={styles.link}>
+            <li
+              className={`${styles.categoryItem} ${
+                selectedCategory === cat ? styles.active : ""
+              }`}
+              onClick={() => {
+                setSelectedCategory(cat);
+              }}
+            >
+              {cat === "home" ? "Home" : cat}
+            </li>
+          </Link>
         ))}
       </ul>
     </aside>
